ac8a1f7b499dde8423e5cb58a66415aa08463262
drhie <rhie.david@gmail.com>
2019-11-25 21:49:47 +0900

    Add CSV parsing module and report storing logic in TimeReport

---
+++ app/models/modules/csv_parser.rb
@@ -0,0 +1,35 @@
+module CsvParser
+  require 'csv'
+
+  def import(file)
+    csv = CSV.parse(File.read(file), headers: true)
+    OpenStruct.new({
+      headers: headers(csv),
+      rows: rows(csv),
+      footer: footer(csv)
+    })
+  end
+
+  private
+  def attribute_hash(row)
+    row.map { |data| [attr_key(data[0]), data[1]] }.to_h.symbolize_keys
+  end
+
+  def attr_key(key)
+    key.strip.gsub(' ', '_')
+  end
+
+  def headers(csv)
+    csv.headers.map { |header| attr_key(header) }
+  end
+
+  def rows(csv)
+    csv.reject { |row| row.to_a.flatten.include?(nil) }
+       .map    { |row| attribute_hash(row) }
+  end
+
+  def footer(csv)
+    footer_row = csv.to_a.last.compact
+    attribute_hash([footer_row])
+  end
+end
--- app/models/time_report.rb
+++ app/models/time_report.rb
@@ -1,3 +1,19 @@
+require_relative 'modules/csv_parser.rb'
+
 class TimeReport < ApplicationRecord
 	has_many :time_entries
+  validates_uniqueness_of :report_id
+
+  include CsvParser
+
+  def store_report(file)
+    report = self.import(file)
+    self.report_id = report.footer[:report_id]
+    archive(report.rows)
+  end
+
+  private
+  def archive(rows)
+    time_entries.new(rows)
+  end
 end
