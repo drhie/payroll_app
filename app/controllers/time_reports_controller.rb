fde04453a6ac9c6945e5d297927ab5395c11e432
drhie <rhie.david@gmail.com>
2019-11-25 22:07:31 +0900

    Connect routes, controller and views

---
+++ app/controllers/time_reports_controller.rb
@@ -0,0 +1,22 @@
+class TimeReportsController < ApplicationController
+  def create
+    time_report = TimeReport.new
+    time_report.store_report(csv_file)
+    if time_report.valid?
+      time_report.save
+      flash[:success] = "Your report has been saved!"
+    else
+      flash[:errors] = time_report.errors.full_messages
+    end
+    redirect_to payroll_reports_url
+  end
+
+  private
+  def time_report_params
+    params.require(:time_report).permit(:csv_file)
+  end
+
+  def csv_file
+    time_report_params[:csv_file]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'payroll_reports#index'
+  resources :time_reports, only: [:create]
 end
